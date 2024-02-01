8c1a92f97a1703cc3e7e763a1cb665d7d341130a
Verolian <verolianopiyo@gmail.com>
2024-02-01 17:19:59 +0300

    Rectangle #0

---
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,8 @@
+// Rectangle class definition
+class Rectangle {
+    // Empty class, no constructor or methods defined
+  }
+  
+  // Example usage:
+  const myRectangle = new Rectangle();
+  console.log(myRectangle); // Output: Rectangle {}
