9b1aff6b61dce559c7a7d4be21125a48b13f3f1d
Verolian <verolianopiyo@gmail.com>
2024-02-01 18:02:27 +0300

    Square #0

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,20 @@
+// 4-rectangle.js
+
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+        return {};
+      }
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  // Square class definition
+  class Square extends Rectangle {
+    constructor(size) {
+      // Call the constructor of the parent class (Rectangle)
+      super(size, size);
+    }
+  }
+  
