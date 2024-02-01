3992be61a97cd71807c915409f10c55c188f936d
Verolian <verolianopiyo@gmail.com>
2024-02-01 17:53:07 +0300

    Rectangle #3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,19 @@
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        // Create an empty object if w or h is invalid
-        return {};
-      }
-  
-      this.width = w;
-      this.height = h;
+#!/usr/bin/node
+class Rectangle{
+    constructor(w,h){
+    if (w>0 && h>0){
+        this.width = w;
+        this.height = h;
+    };
     }
-  
     print() {
-      if (this.width && this.height) {
-        // Print the rectangle using X characters
+        const p = 'X'
         for (let i = 0; i < this.height; i++) {
-          console.log("X".repeat(this.width));
-        }
-      } else {
-        console.log("Invalid rectangle");
-      }
-    }
-  }
+            console.log('X'.repeat(this.width));
+        };
+        };
+};
+module.exports = Rectangle;
+
+
+module.exports=Rectangle ;
