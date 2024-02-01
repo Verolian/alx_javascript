347e66a33d1c4ef83f8ca946471e9eada7ab109e
Verolian <verolianopiyo@gmail.com>
2024-02-01 17:08:47 +0300

    Object

--- javascript-warm_up/12-object.js
+++ javascript-warm_up/12-object.js
@@ -1,14 +1,10 @@
-// 12-object.js
-
-#!/usr/bin/node
 const myObject = {
-  type: 'object',
-  value: 12
-};
-
-console.log(myObject);
-
-// Update the value property to 89
-myObject.value = 89;
-
-console.log(myObject);
+    type: 'object',
+    value: 12
+  };
+  console.log(myObject);
+  myObject.value=89;/**You can use the const keyword to declare a 
+  * constant, but the properties of an 
+  * object declared within const 
+  * can still be modified.   */
+  console.log(myObject);
