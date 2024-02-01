75029bd2b2ac35194b80c3b8db8483aaba478cac
Verolian <verolianopiyo@gmail.com>
2024-02-01 18:06:52 +0300

    Square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,21 +1,8 @@
-// 4-rectangle.js
-
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        return {};
-      }
-      this.width = w;
-      this.height = h;
-    }
+//super and importing the class rectangle
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor(size) {
+    super(size, size);
   }
-  
-  // Square class definition
-  class Square extends Rectangle {
-    constructor(size) {
-      // Call the constructor of the parent class (Rectangle)
-      super(size, size);
-    }
-  }
-  module.exports = Square;
-  
+ };
+ module.exports = Square;
