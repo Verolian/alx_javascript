970acbee18a92ea749cfd7c871a63e5c47da3c2d
Verolian <verolianopiyo@gmail.com>
2024-02-01 17:47:11 +0300

    Rectangle #2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-        // Create an empty object if w or h is invalid
-        return {};
-      }
-  
-      this.width = w;
-      this.height = h;
-    }
-  }
+class Rectangle{
+    constructor(w,h){
+    if (w>0 && h>0)
+        {this.width=w;
+    this.height=h;}
+        }
+    
+}
 
-  module.exports=Rectangle
+module.exports=Rectangle
 /*const r1 = new Rectangle(2, 3);
 console.log(r1);
 console.log(r1.width);
