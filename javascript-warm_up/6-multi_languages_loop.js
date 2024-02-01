941314452d4b14d382e97fc1a12487896e9efbb3
Verolian <verolianopiyo@gmail.com>
2024-02-01 16:54:00 +0300

    Loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+// 6-multi_languages_loop.js
+
+// Array of strings
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+// Loop through the array and print each line using console.log(...)
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
