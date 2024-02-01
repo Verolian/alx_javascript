e036b9dc1b20950dfac6d74739caca5483043408
Verolian <verolianopiyo@gmail.com>
2024-02-01 17:34:23 +0300

    Rectangle #2

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -8,8 +8,3 @@ class Rectangle {
     }
   }
   module.exports=Rectangle
-
-  // Example usage:
-const myRectangle = new Rectangle(5, 8);
-console.log(myRectangle.width); // Output: 5
-console.log(myRectangle.height); // Output: 8
