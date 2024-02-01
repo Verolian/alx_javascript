0683919224ab672c2311001a1d770e987e0e2f93
Verolian <verolianopiyo@gmail.com>
2024-02-01 17:29:42 +0300

    Rectangle #2

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,14 @@
+// Rectangle class definition
+class Rectangle {
+    // Constructor
+    constructor(w, h) {
+      // Initialize instance attributes
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  // Example usage:
+  const myRectangle = new Rectangle(5, 8);
+  console.log(myRectangle.width); // Output: 5
+  console.log(myRectangle.height); // Output: 8
