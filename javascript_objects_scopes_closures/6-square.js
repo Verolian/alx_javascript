4ea10271e73dbd5c703891dcb67827dd95f99e79
Verolian <verolianopiyo@gmail.com>
2024-02-01 18:09:32 +0300

    Square #1

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,31 @@
+// 5-square.js
+
+class Square {
+    constructor(size) {
+      if (size <= 0 || !Number.isInteger(size)) {
+        return {};
+      }
+      this.width = size;
+      this.height = size;
+    }
+  
+    // Instance method to print the square using the character c (default is X)
+    charPrint(c) {
+      if (c === undefined) {
+        c = 'X'; // Default character is X
+      }
+  
+      // Print the square using the specified character c
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+  
+  // Square class definition, inherits from Square in 5-square.js
+  class Square extends Square {
+    constructor(size) {
+      // Call the constructor of the parent class (Square)
+      super(size);
+    }
+  }
