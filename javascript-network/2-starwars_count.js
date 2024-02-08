babca6d1184ac9ffdbb1968849009a5fb8fa5fe5
Verolian <verolianopiyo@gmail.com>
2024-02-08 21:36:08 +0300

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,34 +1,16 @@
+#!/usr/bin/node 
+
 const request = require('request');
-
-const characterId = 18; // Wedge Antilles ID
-const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
-
-async function countWedgeAppearances(url) {
-  // Fetch film data
-  const response = await request({
-    url,
-    json: true, // Automatically parse JSON response
-  });
-
-  if (response.error) {
-    console.error('Error:', response.error.message);
-    return;
-  }
-
-  const { results: films } = response.body; // Destructure films array
-
-  let appearances = 0;
-
-  // Loop through each film
-  for (const film of films) {
-    // Check if Wedge Antilles is in the characters array
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}`)) {
-      appearances++;
-    }
-  }
-
-  console.log(`Wedge Antilles appearances: ${appearances}`);
-}
-
-// Start recursively fetching from the provided URL
-countWedgeAppearances(baseUrl);
+request(process.argv[2], 
+    function (error, response, body) {
+    
+    if (!error) {
+        const results = JSON.parse(body).results;
+        console.log(results.reduce((count, movie) => {
+          return movie.characters.find((character) => character.endsWith('/18/'))
+            ? count + 1
+            : count;
+        }, 0));
+      }
+    });
+    
